953d5f7a1709b3d0ade9281dd4926411104a6b9c
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-12-26 20:16:43 +0100

    Add CompetitionCard component to home page layout and update template

--- src/app/layouts/home-page/home-page.component.ts
+++ src/app/layouts/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CompetitionCardComponent } from '../../components/cards/competition-card/competition-card.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CompetitionCardComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
