9df2e33139615e5e2d5a0dcfa3abf86015b9e9ad
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-12-26 23:17:08 +0100

    Implement dynamic competition card rendering with data fetching and enhance layout structure

--- src/app/components/cards/competition-card/competition-card.component.ts
+++ src/app/components/cards/competition-card/competition-card.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-competition-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule , HttpClientModule],
   templateUrl: './competition-card.component.html',
   styleUrl: './competition-card.component.css'
 })
-export class CompetitionCardComponent {
+export class CompetitionCardComponent implements OnInit {
+  httpClient = inject(HttpClient);
+  competitions :any[] = [];
+
+  ngOnInit(): void {
+     this.fetchData();
+  }
+
+  fetchData(){
+    this.httpClient.get('http://localhost:8443/api/competition/details').subscribe((competitions : any)=> {
+      console.log(competitions);
+      this.competitions = competitions.content;
+    })
+  }
 
 }
--- src/app/layouts/home-page/home-page.component.ts
+++ src/app/layouts/home-page/home-page.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  , inject} from '@angular/core';
 import { CompetitionCardComponent } from '../../components/cards/competition-card/competition-card.component';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+interface Competition {
+  id: string;
+  code: string;
+  location: string;
+  date: string;
+  speciesType: string;
+  minParticipants: number;
+  maxParticipants: number;
+  openRegistration: boolean;
+}
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CompetitionCardComponent],
+  imports: [CompetitionCardComponent ,
+            CommonModule ,
+            HttpClientModule ,
+            ReactiveFormsModule,
+            FormsModule
+    ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+
+
 
 }
