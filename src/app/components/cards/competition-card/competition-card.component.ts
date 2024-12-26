e764b606ffed3baae5c9ea7c58b37b45485ed8ca
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-12-26 20:03:37 +0100

    Add CompetitionCard component with initial template, styles, and tests

---
+++ src/app/components/cards/competition-card/competition-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competition-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './competition-card.component.html',
+  styleUrl: './competition-card.component.css'
+})
+export class CompetitionCardComponent {
+
+}
