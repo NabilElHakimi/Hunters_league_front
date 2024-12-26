a3caf776a878b639fbb249f09aaf81342f37ce50
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-12-26 15:32:51 +0100

    Enhance login form styling and add register form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/headerfooter/header/header.component';
 import { FooterComponent } from './components/headerfooter/footer/footer.component';
 import { LoginFormComponent } from './components/auth/login-form/login-form.component';
+import { RegisterFormComponent } from './components/auth/register-form/register-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HeaderComponent , FooterComponent , LoginFormComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginFormComponent, RegisterFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
