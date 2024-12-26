39da3c5b6ce2dbeb543557a06b6729feafe7e6c5
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-12-26 15:17:51 +0100

    Add login and register form components with basic templates and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/headerfooter/header/header.component';
+import { FooterComponent } from './components/headerfooter/footer/footer.component';
+import { LoginFormComponent } from './components/auth/login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , HeaderComponent , FooterComponent , LoginFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Hunters_league_front';
 }
-  
