b88d4fa611144493c4e11cfe7e15b68925005e7f
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-12-30 21:17:21 +0100

    Implement AuthService with login functionality and create unit tests for the service

--- src/app/components/auth/login-form/login-form.component.ts
+++ src/app/components/auth/login-form/login-form.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http'
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthServiceService } from '../../../services/auth-service.service';
 
 @Component({
   selector: 'app-login-form',
@@ -12,32 +13,12 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent {
-  constructor(private http: HttpClient, private router: Router) {}
+
+  constructor(private http :HttpClient , private router :Router){}
+
+  service : AuthServiceService  = new AuthServiceService (this.http , this.router);
 
   onSubmit(formData: any) {
-    console.log('Form Data Submitted:', formData);
-
-    const url = 'http://localhost:8443/api/auth/login';
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      }),
-    };
-
-    this.http.post<{ token: string }>(url, formData, httpOptions).subscribe({
-      next: (response) => {
-        console.log('Login successful:', response);
-
-        localStorage.setItem('authToken', response.token);
-
-        this.router.navigate(['/home']);
-        alert('Login successful');
-      },
-      error: (error) => {
-        console.error('Login failed:', error);
-        alert('Login failed. Please check your credentials.');
-      },
-    });
+    this.service.login(formData);
   }
 }
---
+++ src/app/services/auth-service.service.ts
@@ -0,0 +1,47 @@
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
+export class AuthServiceService {
+
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  url = "http://localhost:8443/api/auth/"
+
+  login(formData : FormsModule) {
+      const loginUrl = this.url+"login";
+      console.log(loginUrl);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+
+
+    this.http.post<{ token: string }>(loginUrl, formData, httpOptions).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+
+        localStorage.setItem('authToken', response.token);
+
+        this.router.navigate(['/home']);
+        alert('Login successful');
+      },
+      error: (error) => {
+        console.error('Login failed:', error);
+        alert('Login failed. Please check your credentials.');
+      },
+    });
+
+  }
+
+}
