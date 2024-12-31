b8fd477a9ae1e804c6921deeb88bb9917decb6c0
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-12-31 15:30:16 +0100

    Add scroll-to-top button functionality, update background image, and enhance layout in welcome card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,43 @@
-  import { Component, OnInit } from '@angular/core';
+  import { Component, HostListener, OnInit } from '@angular/core';
   import { Router, RouterOutlet } from '@angular/router';
   import { HeaderComponent } from './components/headerfooter/header/header.component';
   import { FooterComponent } from './components/headerfooter/footer/footer.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { CommonModule } from '@angular/common';
 
   @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent , CommonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     providers: [
       {
         provide: HTTP_INTERCEPTORS,
         useClass: AuthInterceptor,
-        multi: true, 
+        multi: true,
       },
     ],
   })
   export class AppComponent {
     title = 'Hunters_league_front';
+
+    showScrollToTop = false; // Flag to control button visibility
+
+    @HostListener('window:scroll', [])
+    onScroll(): void {
+      // Check if scrolled past 100% of the viewport height
+      const scrolledPastFirstScreen = window.scrollY > window.innerHeight;
+      this.showScrollToTop = scrolledPastFirstScreen; // Show button only if scrolled past the first screen
+    }
+
+    // Scroll to the top of the page
+    scrollToTop(): void {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Smooth scrolling
+      });
+    } 
 
   }
