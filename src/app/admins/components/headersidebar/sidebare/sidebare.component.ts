38c10d00b6d27c3d566255da20a4d0e647ce3ab5
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-01-07 17:17:29 +0100

    Update header and sidebar components to use router links, modify modal title, and enhance competition page layout

--- src/app/admins/components/headersidebar/sidebare/sidebare.component.ts
+++ src/app/admins/components/headersidebar/sidebare/sidebare.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebare',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebare.component.html',
   styleUrl: './sidebare.component.css'
 })
