387e51af7e3a2eec19696ea9868845e32858071c
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-01-08 14:14:32 +0100

    Enhance sidebar functionality with toggle feature and improve user table layout

--- src/app/admins/components/headersidebar/sidebare/sidebare.component.ts
+++ src/app/admins/components/headersidebar/sidebare/sidebare.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebare',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink , CommonModule],
   templateUrl: './sidebare.component.html',
   styleUrl: './sidebare.component.css'
 })
 export class SidebareComponent {
+
+  isClosed = false;
+
+  toggleSidebar() {
+    this.isClosed = !this.isClosed;
+  }
 
 }
