19334fc3fe79f2ba4d07509777e46cb53a2a2028
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-01-08 12:18:43 +0100

    Add UserService and integrate user fetching in users page component

---
+++ src/app/admins/services/user-service/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http : HttpClient ) { }
+
+  getUsers(page: number) {
+    return this.http.get(environment.apiUrl+ '/user/getAll?page=' + page);
+  }
+
+}
--- src/app/admins/views/users-page/users-page.component.ts
+++ src/app/admins/views/users-page/users-page.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { UserService } from './../../services/user-service/user.service';
+import { Component, OnInit } from '@angular/core';
 import { LoaderAdminComponent } from '../../../components/loader-admin/loader-admin.component';
 import { CompetitionCardComponent } from "../../../components/cards/competition-card/competition-card.component";
 import { LoaderComponent } from "../../../components/loader/loader.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users-page',
   standalone: true,
-  imports: [LoaderAdminComponent],
+  imports: [LoaderAdminComponent , CommonModule],
   templateUrl: './users-page.component.html',
   styleUrl: './users-page.component.css'
 })
-export class UsersPageComponent {
+export class UsersPageComponent implements OnInit {
+  isLoading: boolean = false;
+  data : any = [];
+  constructor(private userService : UserService) {}
+  ngOnInit(): void {
+    this.getUsers();
+    this.isLoading = false;
+  }
+
+
+
+
+
+  getUsers() {
+    this.userService.getUsers(0).subscribe((res: any) => {
+      console.log(res);
+      this.data = res.content;
+      console.log(this.data);
+      });
+  }
 
 }
